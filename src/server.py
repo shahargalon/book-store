b12a74b76050cb0d6ae2715bbd3f4c7df97c21ff
Nir Galon <nir@galon.io>
2020-11-23 10:56:38 +0200

    (feat): Add DB

--- src/server.py
+++ src/server.py
@@ -1,7 +1,35 @@
+from peewee import *
 from sanic import Sanic
 from sanic.response import json
 
 app = Sanic(name="awesome_book_store")
+psql_db = PostgresqlDatabase('book_store', user='nirgalon')
+
+
+class Person(Model):
+    first_name = CharField()
+    last_name = CharField()
+    age = IntegerField()
+
+    class Meta:
+        database = psql_db
+
+
+@app.route('/person', methods=["POST"])
+async def create_person(request):
+    new_person = Person(
+        first_name=request.json["first_name"],
+        last_name=request.json["last_name"],
+        age=int(request.json["age"]),
+    )
+    new_person.save()
+    return json(new_person)
+
+
+@app.route('/person',)
+async def create_person(request):
+    persons = Person.select()
+    return json(persons)
 
 
 @app.route('/')
